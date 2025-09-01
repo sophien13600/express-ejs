fb3239e2743a697fe64d87f5f0900bd85043d996
Sophien <bouderbala.s@gmail.com>
2025-09-01 14:53:10 +0200

    configuration route

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import 'dotenv/config'
+import personne from './routes/personne.route.js'
+import adresse from './routes/adresse.route.js'
+
+const app = express()
+// Mapping entre routes et le routeur
+app.use("/personne", personne)
+app.use("/adresse", adresse)
+
+
+app.get(['/', '/home', '/accueil'], (req, res) => {
+    res.end("Hello world!")
+})
+
+
+
+
+app.all("/*splat", (req, res) => {
+    res
+        .status(404)
+        .end("Page introuvable")
+})
+
+
+const PORT = process.env.PORT || 5555
+
+app.listen(PORT, () => {
+    console.log(`Adresse serveur : http://localhost:${PORT}`);
+})
